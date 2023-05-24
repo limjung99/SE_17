a8f4d130af47cd61a7c3e755c9e9292d40c81d60
yuhyeon0809 <uvaaaan0809@naver.com>
2023-05-24 14:31:13 +0900

    신유현_채용정보검색 및 채용정보 헤더추가

---
+++ SRC/SearchRecruitmentUI.h
@@ -0,0 +1,9 @@
+#include "CompanyUser.h"
+#include "SearchRecruitment.h"
+
+class SearchRecruitmentUI {
+        SearchRecruitment* SearchRecruitment;
+    public:
+        void startInterface();
+        void searchRecruitments(string companyName);
+};
