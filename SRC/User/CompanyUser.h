3bcf1edcfba306642e23cc21b465df5b443651f0
lim <limjung99@naver.com>
2023-05-24 21:50:28 +0900

    충돌병합

--- SRC/User/CompanyUser.h
+++ SRC/User/CompanyUser.h
@@ -8,9 +8,13 @@ class CompanyUser:User{
         vector<Recruitment> myrecruitments; /* companyuser가 작성한 채용공고 vector */
         int myRecruitmentNumber; /* 채용공고 숫자 */
     public:
+<<<<<<< Updated upstream
         
         void listRecruitment();
+=======
+        void listRecruitment(); /* companyuser 인스턴스가 소유한 recruitment list */
+>>>>>>> Stashed changes
         void addRecruitment(Recruitment *recruitment); /* companyuser가 작성한 recruitment를 member 배열에 포함시키는 함수*/
         int getRecruitmentsNumber();
-        vector<Recruitment*> getRecruitmentsList(); /* companyuser 인스턴스가 소유한 recruitment인스턴스의 포인터 vector를 반환하는 함수 */
+      
 };
